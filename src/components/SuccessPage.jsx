34e1d416574cf078517734772c33e8a934d60949
VilBRabad <rabadvilas11@gmail.com>
2024-12-14 12:37:39 +0530

    some changes

--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function SuccessPage() {
+    const navigate = useNavigate();
+
     return (
         <div className='w-screen h-screen flex items-center justify-center'>
-            <p>Payment successfully</p>
+            <p className='text-4xl font-bold'>Payment successfully</p>
+            <p onClick={() => navigate("/orders")} className='text-4xl font-bold'>Go to Order</p>
         </div>
     )
 }
